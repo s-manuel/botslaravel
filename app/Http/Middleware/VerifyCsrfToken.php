d9e2fa6190a7e9bd2d98f3ff69453b1be1db1ba2
s-manuel <guangasitsm@outlook.com>
2023-08-25 07:50:55 -0500

    Add: ChatBotTwilio

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        '/chat-bot',
+        '/chat-send'
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatBotTwilio;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,9 +12,17 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
+curl -X POST \
+  -d "message=Hola&recipient=0995819939" \
+  "http://localhost:8000/api/chat-send"
+
+  https://www.twilio.com/es-mx/blog/crear-un-chatbot-de-whatsapp-con-la-api-de-whatsapp-de-twilio-php-y-laravel
 |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/chat-bot', [ChatBotTwilio::class, 'listenToReplies']);
+Route::post('/chat-send', [ChatBotTwilio::class, 'sendWhatsAppMessage']);
